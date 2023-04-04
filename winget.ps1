0518a5d86b2a6a20362177a2bce5b07f13f10696
Jeppe <jeppe@bandsholm.com>
2023-04-04 19:08:08 +0200

    added small test rubber ducky script and experimental winget

---
+++ winget.ps1
@@ -0,0 +1,13 @@
+winget install Microsoft.VisualStudio.2022.Community
+winget install Discord.Discord
+winget install Telerik.Fiddler.Classic
+winget install GitHub.GitHubDesktop
+winget install Git.Git
+winget install Postman.Postman
+winget install NordVPN.NordVPN
+winget install Microsoft.VisualStudioCode
+winget install -e --id Microsoft.PowerToys
+winget install devtoys
+winget install python
+winget install -e --id ColdTurkeySoftware.ColdTurkeyBlocker
+winget install -e --id Valve.Steam
