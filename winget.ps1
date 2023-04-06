99bde79e9dd78050b155de24c10b0632a0fd2da0
Jeppe <jeppe@bandsholm.com>
2023-04-06 14:59:19 +0200

    slight update to install script, and add checks to winget

--- winget.ps1
+++ winget.ps1
@@ -1,13 +1,24 @@
-winget install Microsoft.VisualStudio.2022.Community
-winget install Discord.Discord
-winget install Telerik.Fiddler.Classic
-winget install GitHub.GitHubDesktop
-winget install Git.Git
-winget install Postman.Postman
-winget install NordVPN.NordVPN
-winget install Microsoft.VisualStudioCode
-winget install -e --id Microsoft.PowerToys
-winget install devtoys
-winget install python
-winget install -e --id ColdTurkeySoftware.ColdTurkeyBlocker
-winget install -e --id Valve.Steam
+$packages = @(
+    "Microsoft.VisualStudio.2022.Community",
+    "Discord.Discord",
+    "Telerik.Fiddler.Classic",
+    "GitHub.GitHubDesktop",
+    "Git.Git",
+    "Postman.Postman",
+    "NordVPN.NordVPN",
+    "Microsoft.VisualStudioCode",
+    "Microsoft.PowerToys",
+    "devtoys",
+    "python",
+    "ColdTurkeySoftware.ColdTurkeyBlocker",
+    "Valve.Steam"
+)
+
+foreach ($package in $packages) {
+    if (Get-Package -Name $package -ErrorAction SilentlyContinue) {
+        Write-Host "$package is already installed"
+    } else {
+        Write-Host "$package is not installed, installing now..."
+        winget install $package
+    }
+}
